1b24c010ceb0424d912402858dfb845e0d4bad87
Ritwik Chandra <123593487+Ritwik1607@users.noreply.github.com>
2026-03-13 18:46:30 +0530

    Add files via upload

---
+++ spring-first-app 13-03-2026/src/main/java/com/cg/bean/Employee.java
@@ -0,0 +1,35 @@
+package com.cg.bean;
+
+public class Employee {
+
+	private int empid;
+	private String name;
+	private Address address;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public void printEmployeeDetails() {
+		System.out.println("Employee Id:"+empid);
+		System.out.println("Employee Name:"+name);
+		System.out.println("======Address=========");
+		System.out.println("City:"+address.getCity());
+		System.out.println("Country:"+address.getCountry());
+		System.out.println("zip:"+address.getZip());
+
+	}
+}
---
+++ spring-first-app 13-03-2026/src/main/java/com/cg/main/Main.java
@@ -0,0 +1,20 @@
+package com.cg.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import com.cg.bean.HelloWorld;
+
+public class Main {
+	public static void main(String[]args)
+	{
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("bean.xml");
+		HelloWorld h1=(HelloWorld)ctx.getBean("hello");
+		HelloWorld h2=(HelloWorld)ctx.getBean("hello");
+		//h1.setName("ritwik");
+		System.out.println(h1.sayHello());
+		System.out.println(h2.sayHello());
+		((ClassPathXmlApplicationContext)ctx).close();
+	}
+}
